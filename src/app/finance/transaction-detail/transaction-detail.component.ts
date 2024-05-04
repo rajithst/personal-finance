94fabc31daac9204cb9d0b44a42ea296c919e6f9
rajithst <rajiththennakoonshady@gmail.com>
2024-05-05 00:15:52 +0900

    Implemented transaction merge functionality
    Added transaction merging functionality along with the merging table in the user interface. Made changes to the transaction update dialog for deletion, implemented drag-drop to the dialog header, and modified the user interface by adding tabs in transactions. Furthermore, enhanced API service to accommodate the merging feature.

--- src/app/finance/transaction-detail/transaction-detail.component.ts
+++ src/app/finance/transaction-detail/transaction-detail.component.ts
@@ -1,25 +1,23 @@
-import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
-import {TransactionUpdateDialogData} from "../transaction-update/transaction-update.component";
-import {Transaction} from "../model/transactions";
+import { Component, Inject } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
 
-export interface TransactionDetailDialogData {
-
-}
+export interface TransactionDetailDialogData {}
 
 @Component({
   selector: 'app-transaction-detail',
   templateUrl: './transaction-detail.component.html',
-  styleUrl: './transaction-detail.component.css'
+  styleUrl: './transaction-detail.component.css',
 })
 export class TransactionDetailDialog {
+  constructor(
+    private dialog: MatDialog,
+    public dialogRef: MatDialogRef<TransactionDetailDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: TransactionDetailDialogData,
+  ) {}
 
-  constructor(private dialog: MatDialog,
-              public dialogRef: MatDialogRef<TransactionDetailDialog>,
-              @Inject(MAT_DIALOG_DATA) public data: TransactionDetailDialogData,) {
-  }
-
-  close() {
-
-  }
+  close() {}
 }
