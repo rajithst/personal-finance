8e02fa2d02ec663a56e3c119fffc0258666c945a
rajithst <rajiththennakoonshady@gmail.com>
2024-05-25 23:09:34 +0900

    Moved create new items to submenu bar, style fixes in add trade dialog

--- src/app/finance/submenu/submenu.component.ts
+++ src/app/finance/submenu/submenu.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { faCirclePlus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { TransactionUpdateDialog } from '../transaction-update/transaction-update.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-submenu',
   templateUrl: './submenu.component.html',
-  styleUrl: './submenu.component.css'
+  styleUrl: './submenu.component.css',
 })
 export class SubmenuComponent {
+  protected readonly faCirclePlus = faCirclePlus;
+  protected readonly faPlus = faPlus;
 
+  constructor(private dialog: MatDialog) {}
+
+  addTransaction() {
+    this.dialog.open(TransactionUpdateDialog, {
+      width: '850px',
+      position: {
+        top: '50px',
+      },
+      data: { formData: null, task: 'add' },
+    });
+  }
 }
--- src/app/investments/holdings/holdings.component.ts
+++ src/app/investments/holdings/holdings.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { TradeDialogComponent } from '../trade-dialog/trade-dialog.component';
 import {SessionService} from "../service/session.service";
-import {
-  faCaretUp,
-  faLineChart,
-  faCaretDown,
-  faMoneyBill,
-  faMoneyBillTrendUp, faJpy
-} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-holdings',
@@ -16,35 +9,17 @@ import {
   styleUrl: './holdings.component.css',
 })
 export class HoldingsComponent implements OnInit {
-  protected readonly Math = Math;
-  protected readonly faMoneyBill = faMoneyBill;
-  protected readonly faMoneyBillTrendUp = faMoneyBillTrendUp;
-  protected readonly faLineChart = faLineChart;
-  protected readonly faCaretUp = faCaretUp;
-  protected readonly faCaretDown = faCaretDown;
-  protected readonly faJpy = faJpy;
-
   private sessionData = this.sessionService.getData();
-  constructor(private dialog: MatDialog, private sessionService: SessionService) {}
-  usHoldings: any[] = []
-  domesticHoldings: any[] = []
+  constructor(private sessionService: SessionService) {}
+  usHoldings: any[] = [];
+  domesticHoldings: any[] = [];
 
   ngOnInit(): void {
-    this.usHoldings = this.sessionData.holdings.filter(x => x.stock_currency === '$')
-    this.domesticHoldings = this.sessionData.holdings.filter(x => x.stock_currency === '¥')
+    this.usHoldings = this.sessionData.holdings.filter(
+      (x) => x.stock_currency === '$',
+    );
+    this.domesticHoldings = this.sessionData.holdings.filter(
+      (x) => x.stock_currency === '¥',
+    );
   }
-
-  addTransaction() {
-    this.dialog.open(TradeDialogComponent, {
-      width: '750px',
-      position: {
-        top: '50px',
-      },
-      data: { formData: null, task: 'add' },
-    });
-  }
-
-
-
-
 }
