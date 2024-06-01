0bf55fb50c77320e1fc7e8e0b661b9faa6a2658e
rajithst <rajiththennakoonshady@gmail.com>
2024-06-01 09:16:38 +0900

    moved client to gcp appengine

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://personal-finance-backend-lofxa.ondigitalocean.app'
+  apiUrl: 'https://backend-dot-personal-finance-425009.uc.r.appspot.com'
 };
