00e1f52353dcdd67d20f8ea582f90487556b720d
rajithst <rajiththennakoonshady@gmail.com>
2024-11-28 00:31:34 +0900

    Fixed styling issues and form inputs in import dialogs

--- src/app/investments/holdings/holding-import/holding-import.component.ts
+++ src/app/investments/holdings/holding-import/holding-import.component.ts
@@ -36,6 +36,11 @@ import { ApiService } from '../../../core/api.service';
 import { DataService } from '../../../service/data.service';
 import { ACCOUNT_TYPE_INVESTMENT_ACCOUNT } from '../../../shared/data/client.data';
 import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatProgressBar} from "@angular/material/progress-bar";
+import {HttpEventType} from "@angular/common/http";
+import {throwError} from "rxjs";
+import {CreditAccount} from "../../../finance/model/account";
 
 @Component({
   selector: 'app-holding-import',
@@ -71,6 +76,9 @@ import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
     MatSuffix,
     MatRadioGroup,
     MatRadioButton,
+    MatCard,
+    MatCardContent,
+    MatProgressBar,
   ],
   providers: [provideNativeDateAdapter()],
 })
@@ -78,8 +86,16 @@ export class HoldingImportComponent {
   private readonly apiService = inject(ApiService);
   private readonly dataService = inject(DataService);
 
+  myAccounts: any[] = this.dataService
+    .getAccounts()
+    .filter((x) => x.account_type === ACCOUNT_TYPE_INVESTMENT_ACCOUNT);
+  progress = 0;
+  clickSubmit = false;
+  uploadComplete = false;
+  files: Array<File> = [];
+
   accountForm = new FormGroup({
-    account: new FormControl<number | null>(null),
+    account: new FormControl<CreditAccount | null>(null),
   });
   secondFormGroup = new FormGroup({
     target: new FormControl<string | null>(null),
@@ -89,14 +105,19 @@ export class HoldingImportComponent {
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
   });
-  otherInfoForm = new FormGroup({
-    drop_duplicates: new FormControl<boolean>(true),
-    from_last_import_date: new FormControl<boolean>(false),
-  });
-  myAccounts: any[] = this.dataService
-    .getAccounts()
-    .filter((x) => x.account_type === ACCOUNT_TYPE_INVESTMENT_ACCOUNT);
-  files: Array<File> = [];
+
+  get selectedAccountName() {
+    return this.accountForm.get('account')?.value?.account_name ?? '';
+  }
+  get selectedTargetName() {
+    return this.secondFormGroup.get('target')?.value ?? '';
+  }
+  get importFirstDate() {
+    return `${moment(this.rangeForm.get('start')?.value).format('YYYY-MM-DD').toString()}`
+  }
+  get importLastDate() {
+    return `${moment(this.rangeForm.get('end')?.value).format('YYYY-MM-DD').toString()}`;
+  }
 
   onChange(event: any) {
     const uploadHook = event.target as HTMLInputElement;
@@ -113,25 +134,31 @@ export class HoldingImportComponent {
     this.files.forEach((x) => {
       formData.append('files', x);
     });
-    const accountId = this.accountForm.get('account')?.value;
-    const dt1 = this.rangeForm.get('start')?.value;
-    const dt2 = this.rangeForm.get('end')?.value;
-    let importStartDate = '';
-    let importEndDate = '';
-    if (dt1) {
-      importStartDate = moment(dt1).format('YYYY-MM-DD').toString();
-    }
-    if (dt2) {
-      importEndDate = moment(dt2).format('YYYY-MM-DD').toString();
-    }
-
-    formData.append('account_id', accountId!.toString());
+    const accountId = this.accountForm.get('account')?.value?.id.toString() ?? '';
+    const importStartDate = this.importFirstDate ?? '';
+    const importEndDate = this.importLastDate ?? '';
+    const target = this.secondFormGroup.get('target')?.value ?? '';
+    formData.append('account_id', accountId);
     formData.append('start_date', importStartDate);
     formData.append('end_date', importEndDate);
+    formData.append('target', target);
     const upload$ = this.apiService.uploadHoldingTransactions(formData);
     upload$.subscribe({
-      next: () => {},
-      error: (error: any) => {},
+      next: (event) => {
+        switch (event.type) {
+          case HttpEventType.UploadProgress:
+            if (event.total) {
+              this.progress = Math.round((event.loaded / event.total) * 100);
+            }
+            break;
+          case HttpEventType.Response:
+            this.progress = 100;
+            this.uploadComplete = true;
+        }
+      },
+      error: (error: any) => {
+        return throwError(() => error);
+      },
     });
   }
 
@@ -140,7 +167,7 @@ export class HoldingImportComponent {
   }
 
   isValidToSubmit() {
-    return false;
+    return !this.accountForm.invalid && this.files.length > 0;
   }
 
   cancel() {}
